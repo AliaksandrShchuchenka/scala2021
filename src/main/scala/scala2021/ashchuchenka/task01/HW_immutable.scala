a91085b640121f1c4d59c7c4db9519d71f8c8dd5
Aliaksandr Shchuchenka <ashchuchenka@deloitte.com>
2021-04-21 17:52:32 +0300

    First Task is implemented and tested

---
+++ src/main/scala/scala2021/ashchuchenka/task01/HW_immutable.scala
@@ -0,0 +1,34 @@
+package scala2021.ashchuchenka.task01
+
+import scala.collection.immutable
+
+object HW_immutable {
+
+  def processDomain(qty: Int, array: Array[String]): Array[(Int, String)] = {
+
+    val domainStr = array.mkString(".")
+    val result: Array[(Int, String)] = Array((qty,domainStr))
+
+    if (array.length > 1) {
+      result ++ processDomain(qty, array.drop(1))
+    }
+    else result
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val counts = Array(
+      "900,google.com",
+      "60,mail.yahoo.com",
+      "10,mobile.sports.yahoo.com",
+      "40,sports.yahoo.com",
+      "10,stackoverflow.com",
+      "2,en.wikipedia.org",
+      "1,es.wikipedia.org",
+      "1,mobile.sports"
+    )
+    val ds = counts.map(r => r.split(',')).flatMap{a => processDomain(a(0).toInt,a(1).split('.'))}
+    val res = ds.groupBy(_._2).map(r => (r._1, r._2.map(a => a._1).toList.sum))
+    res.foreach(r => println(r._2+" "+r._1))
+  }
+}
