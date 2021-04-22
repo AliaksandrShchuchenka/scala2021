bbd1464a79d704919e5615a9a9bbc4c2112b4640
Aliaksandr Shchuchenka <ashchuchenka@deloitte.com>
2021-04-22 18:00:55 +0300

    Code clearing

--- src/main/scala/scala2021/ashchuchenka/task02/HW.scala
+++ src/main/scala/scala2021/ashchuchenka/task02/HW.scala
@@ -7,26 +7,27 @@ object HW {
   def calcBrackets(str: String): Boolean = {
 
     @tailrec
-    def calcBracketsHelper(l: List[Char], b: Int): Boolean = {
+    def calcBracketsHelper(l: List[Char], b: Int): Boolean =
 
       l match {
         case _ if b < 0 => false
         case Nil => b == 0
-        case head::tail =>
-            val i = head match {
-              case '(' => 1
-              case ')' => -1
-              case _ => 0
-            }
-            calcBracketsHelper(tail, b + i)
+        case head::tail => {
+          val i = head match {
+            case '(' => 1
+            case ')' => -1
+            case _ => 0
+          }
+          calcBracketsHelper(tail, b + i)
+        }
       }
-    }
+
 
     calcBracketsHelper(str.toList, 0)
   }
 
   def main(args: Array[String]): Unit = {
-    val str = "())("
+    val str = ":-)"
     println(calcBrackets(str))
   }
 }
