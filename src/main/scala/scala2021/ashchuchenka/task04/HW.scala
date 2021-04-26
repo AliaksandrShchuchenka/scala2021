feb5858d69da3469ce85e57a9d73f8b0041e508e
Aliaksandr Shchuchenka <ashchuchenka@deloitte.com>
2021-04-26 18:15:39 +0300

    Task 4 is optimized

--- src/main/scala/scala2021/ashchuchenka/task04/HW.scala
+++ src/main/scala/scala2021/ashchuchenka/task04/HW.scala
@@ -5,26 +5,31 @@ import scala.annotation.tailrec
 object HW {
 
   def checkSum(l: List[Int], x: Int): Boolean = {
-    if (l.contains(x))
-      true
-    else if (l.min > x)
-      false
+    if (l.isEmpty && x == 0) true
+    else if (l.isEmpty && x != 0) false
     else {
-      @tailrec
-      def calcB(l: List[Int], x: Int, acc: List[Int]): Boolean = {
-        val res = acc.flatMap(av => l.map(lv => av + lv))
-        if (res.contains(x)) true
-        else if (res.min < x) calcB(l, x, res)
-        else false
-      }
 
-      calcB(l, x, l)
+      if (l.contains(x))
+        true
+      else if (l.min > x)
+        false
+      else {
+        @tailrec
+        def calcB(l: List[Int], x: Int, acc: List[Int]): Boolean = {
+          val res = acc.flatMap(av => l.map(lv => av + lv))
+          if (res.contains(x)) true
+          else if (res.min < x) calcB(l, x, res)
+          else false
+        }
+
+        calcB(l, x, l)
+      }
     }
   }
 
   def main(args: Array[String]): Unit = {
-    val l = List(2,4,6)
-    val x = 5
+    val l = List()
+    val x = 0
     println(checkSum(l, x))
   }
 }
