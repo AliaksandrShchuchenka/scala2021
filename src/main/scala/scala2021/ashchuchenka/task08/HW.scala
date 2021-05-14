e606e38f39222ee6c5933d00feef30e0ff4df51f
Aliaksandr Shchuchenka <ashchuchenka@deloitte.com>
2021-05-14 10:42:43 +0300

    Task 08 Refactoring 2

--- src/main/scala/scala2021/ashchuchenka/task08/HW.scala
+++ src/main/scala/scala2021/ashchuchenka/task08/HW.scala
@@ -1,43 +1,49 @@
 package scala2021.ashchuchenka.task08
 
+import scala.collection.immutable.ArraySeq
+
 object HW {
+  def stabilizeScoreStrLength(strScore: String): Array[String] = {
+    val scoreArr = strScore
+      .split('|')
+
+    val EmptyBonusChecked = scoreArr.length match {
+      case 10 => scoreArr :+ "00"
+      case _ => scoreArr
+    }
+
+    EmptyBonusChecked.last.length match {
+      case 2 => EmptyBonusChecked
+      case 1 => EmptyBonusChecked.take(EmptyBonusChecked.length-1):+(EmptyBonusChecked.last+"0")
+    }
+  }
 
   def calcScore(strScore: String): Int = {
-    val v = strScore
-      .split('|')
-      .toList
-
-    val n = v.length match {
-      case 10 => v :+ "00"
-      case _ => v
-    }
-
-    val q = n.last.length match {
-      case 2 => n
-      case 1 => n.take(n.length-1):+(n.last+"0")
-    }
 
     val p =
       (
         (
-          q
-            .map(n => n.toList.map(_.toString))
-            .map(n => n.map(nn => if (nn == "-") "0" else nn))
-            .map(n => n.map(nn => if (nn == "X") "10" else nn))
-            .map(l => if ((l.length == 2) && l(1) == "/") List(l(0), (-1 * (10 - l(0).toInt)).toString) else l)
+          stabilizeScoreStrLength(strScore)
+            .map(n => n.map(_.toString))
+            .map(n => n.map(nn => if (nn == "-") "0" else nn)) //Replace "-" to 0 scores
+            .map(n => n.map(nn => if (nn == "X") "10" else nn)) //Replace "X" to 10 scores
+            //Replace "/" to scores with "-" sign
+            .map(l => if ((l.length == 2) && l(1) == "/") ArraySeq(l(0), (-1 * (10 - l(0).toInt)).toString) else l)
             .flatMap(l => l)
             .map(l => l.toInt)
-          )
-          :+ 0
         )
+          :+ 0 //Add extra element to Array (for avoiding lost elements after sliding
+      )
         .map(l => (l, 0))
-        .sliding(2).toList
-        .map(l => if (l.head._1 < 0) (-1 * l.head._1, l(1)._1) else l(0))
-        .sliding(3).toList
-        .map(l => (if(l(0)._1 == 10) (l(0)._1,l(1)._1+l(2)._1) else l(0)))
+        .sliding(2)
+        //replace all nubmers with "-" sign to scores (continue processing of '/' elements)
+        .map(l => if (l.head._1 < 0) (-1 * l.head._1, l(1)._1) else l.head)
+        .sliding(3)
+        //continue processing of 'X' elements
+        .map(l => (if(l.head._1 == 10) (l.head._1,l(1)._1+l(2)._1) else l.head))
         .map(l => l._1+l._2)
         .sum
-     p
+    p
   }
 
   def main(args: Array[String]): Unit = {
